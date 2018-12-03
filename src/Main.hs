8a42605771aadb8475c68baeff32abff413dd367
William Casarin <jb55@jb55.com>
2018-12-02 18:32:56 -0800

    serve static

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,7 @@ module Main where
 import Web.Scotty
 import System.Environment
 import Lucid
+import Network.Wai.Middleware.Static (staticPolicy, addBase)
 import Text.Read (readMaybe)
 import Data.Maybe (fromMaybe)
 
@@ -14,7 +15,7 @@ getPort = do
   mstrport <- lookupEnv "PORT"
   return (fromMaybe 8002 (mstrport >>= readMaybe))
 
-template title = do
+template title contents = do
   doctype_
   html_ $ do
     head_ $ do
@@ -22,13 +23,16 @@ template title = do
       meta_ [ name_ "viewport", content_ "width=device-width, initial-scale=1" ]
       link_ [ rel_ "stylesheet", href_ "css/normalize.css" ]
       link_ [ rel_ "stylesheet", href_ "css/skeleton.css" ]
+    body_ $ do
+      div_ [ class_ "container" ] $ do
+        contents
   where
     renderedTitle =
       maybe "bitsbacker" ("bitsbacker | " <>) title
 
 home = do
-  template Nothing
-  body_ (h1_ "Hello, world!")
+  template Nothing $ do
+    h1_ "Hello, world!"
 
 postSignup = do
   name  <- param "name"
@@ -36,19 +40,25 @@ postSignup = do
   text (name <> email)
 
 signup = do
-  template (Just "signup")
-  body_ $ do
+  template (Just "signup") $ do
     h1_ "Signup"
-    input_ [ id_ "name" ]
-    input_ [ id_ "email" ]
+    form_ $ do
+      label_ [ for_ "name" ] "Name"
+      input_ [ id_ "name" ]
+      label_ [ for_ "email" ] "Email"
+      input_ [ id_ "email" ]
 
 
 content = html . renderText
+
+static path =
+  staticPolicy (addBase path)
 
 routes = do
   get  "/"       (content home)
   get  "/signup" (content signup)
   post "/signup" postSignup
+  middleware (static "public")
 
 main :: IO ()
 main = do
