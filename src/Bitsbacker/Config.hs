17c94ef2b1b30b23c4baf6f1d818cede4bec4f93
William Casarin <jb55@jb55.com>
2018-12-24 11:50:10 -0500

    get peer info from clightning getinfo

--- src/Bitsbacker/Config.hs
+++ src/Bitsbacker/Config.hs
@@ -8,12 +8,15 @@ import Control.Concurrent.MVar
 import Control.Concurrent (forkIO)
 import Control.Exception (SomeException, try)
 import Data.Functor (void)
+import Data.Text (Text)
 import Data.Maybe (fromMaybe, isJust)
 import Database.SQLite.Simple (Connection, execute, query_, Only(..))
 import Network.RPC (rpc)
 import System.Environment (lookupEnv)
 import System.Timeout (timeout)
 import Data.Aeson
+import Data.Aeson.Types
+import Data.List.NonEmpty (NonEmpty)
 
 import Bitcoin.Network
 import Bitsbacker.DB
@@ -25,28 +28,76 @@ import Network.RPC
 import Network.RPC.Config (SocketConfig(..))
 
 import qualified Data.HashMap.Lazy as Map
+import qualified Data.List.NonEmpty as NE
+import qualified Data.Text as T
 
 data Config = Config {
       cfgConn      :: MVar Connection
     , cfgRPC       :: SocketConfig
     , cfgPayNotify :: MVar WaitInvoice
-    , cfgNetwork   :: BitcoinNetwork
+    , cfgLnConfig  :: LightningConfig
     }
 
+cfgNetwork :: Config -> BitcoinNetwork
+cfgNetwork = lncfgNetwork . cfgLnConfig
+
+data PeerAddr = PeerAddr {
+      peerAddrType :: Text
+    , peerAddr     :: Text
+    , peerAddrPort :: Int
+    }
+
+instance FromJSON PeerAddr where
+    parseJSON (Object obj) =
+        PeerAddr <$> obj .: "type"
+                 <*> obj .: "address"
+                 <*> obj .: "port"
+    parseJSON _ = fail "expected object for PeerAddr"
+
+
+data LightningConfig = LightningConfig {
+      lncfgNetwork  :: BitcoinNetwork
+    , lncfgPeerId   :: Text
+    , lncfgPeerAddr :: NonEmpty PeerAddr
+    }
+
+instance FromJSON LightningConfig where
+    parseJSON v@(Object obj) =
+        LightningConfig <$> parseNetworkValue v
+                        <*> obj .: "id"
+                        <*> obj .: "address"
+    parseJSON _ = fail "could not parse clightning getinfo config"
+
 instance ToJSON Config where
-    toJSON Config{..} =
-        object
-           [ "network"    .= cfgNetwork
-           , "is_testnet" .= (cfgNetwork == Testnet)
-           ]
+    toJSON cfg@Config{..} =
+        let network = cfgNetwork cfg
+        in
+          object
+            [ "network"    .= network
+            , "is_testnet" .= (network == Testnet)
+            ]
+
+
+showLnPeer :: LightningConfig -> Text
+showLnPeer LightningConfig{..} =
+    showPeer (NE.head lncfgPeerAddr) lncfgPeerId
+        
+showPeer :: PeerAddr -> Text -> Text
+showPeer PeerAddr{..} peerId =
+    if peerAddrPort == 9735
+       then peerId <> "@" <> peerAddr
+       else peerId <> "@" <> peerAddr <> ":" <> T.pack (show peerAddrPort)
+
 
 getPayIndex :: Connection -> IO Int
 getPayIndex conn =
   fromOnly . head <$> query_ conn "SELECT payindex FROM payindex"
 
+
 persistPayIndex :: Connection -> Int -> IO ()
 persistPayIndex conn payind =
   execute conn "UPDATE payindex SET payindex = ?" (Only payind)
+
 
 waitInvoices :: Int -> Int -> Config -> IO ()
 waitInvoices 10 _ _ = fail "Too many errors when waiting for payments"
@@ -64,33 +115,45 @@ waitInvoices !errs !payindex !cfg@Config{..} = do
       withMVar cfgConn $ \conn -> persistPayIndex conn index
       waitInvoices 0 index cfg
 
-getNetwork :: SocketConfig -> IO BitcoinNetwork
-getNetwork cfg = do
-  mval    <- timeout (5 * 1000000) (rpc_ cfg "getinfo")
-  val     <- maybe timeouterr return mval
-  network <- maybe err return (key "network" val)
-  maybe err return (parseNetwork network)
+
+getLightningConfig :: SocketConfig -> IO LightningConfig
+getLightningConfig cfg = do
+  mlncfg <- timeout (5 * 1000000) (rpc_ cfg "getinfo")
+  lncfg <- maybe timeouterr return mlncfg
+  logError $ "[ln] using peer " ++ T.unpack (showLnPeer lncfg)
+  return lncfg
   where
     timeouterr = fail "timeout during clightning getinfo call"
-    err = fail "getinfo: network key not found"
+
+
+parseNetworkValue :: Value -> Parser BitcoinNetwork
+parseNetworkValue = either fail return . getNetwork
+
+getNetwork :: Value -> Either String BitcoinNetwork
+getNetwork val = do
+  network <- key "network" val
+  maybe err return (parseNetwork network)
+  where
+    err = Left "getinfo: network key not found"
     key str (Object obj) =
       case Map.lookup str obj of
-        Just (String txt) -> Just txt
-        Just _            -> Nothing
-        Nothing           -> Nothing
-    key _ _ = Nothing
+        Just (String txt) -> return txt
+        Just _            -> err
+        Nothing           -> err
+    key _ _ = err
 
 getConfig :: IO Config
 getConfig = do
   socketCfg <- getSocketConfig
-  network <- getNetwork socketCfg
-  logError $ "[ln] detected Bitcoin " ++ show network ++ " from clightning"
-  conn <- openDb network
+  lncfg <- getLightningConfig socketCfg
+  logError $ "[ln] detected Bitcoin " ++ show (lncfgNetwork lncfg)
+                 ++ " from clightning"
+  conn <- openDb (lncfgNetwork lncfg)
   migrate conn
   payindex <- getPayIndex conn
   mvconn <- newMVar conn
   mvnotify <- newEmptyMVar
-  let cfg = Config mvconn socketCfg mvnotify network
+  let cfg = Config mvconn socketCfg mvnotify lncfg
   _ <- forkIO (waitInvoices 0 payindex cfg)
   return cfg
 
